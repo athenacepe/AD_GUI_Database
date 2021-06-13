2d56ccbe385063b386e9d471d183dd76568ad850
athenacepe <athcep19@student.wintec.ac.nz>
2021-06-13 18:32:47 +1200

    Initial commit

---
+++ A2_ADB_Athena/A2_AccessDatabase/A2_AccessDatabase/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace A2_AccessDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Input = new System.Windows.Forms.MaskedTextBox();
+            this.Confirm = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input.Location = new System.Drawing.Point(246, 88);
+            this.Input.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(245, 27);
+            this.Input.TabIndex = 0;
+            // 
+            // Confirm
+            // 
+            this.Confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Confirm.Location = new System.Drawing.Point(534, 81);
+            this.Confirm.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Size = new System.Drawing.Size(109, 41);
+            this.Confirm.TabIndex = 1;
+            this.Confirm.Text = "Confirm";
+            this.Confirm.UseVisualStyleBackColor = true;
+            this.Confirm.Click += new System.EventHandler(this.Confirm_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(648, 81);
+            this.Clear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(93, 41);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search Query: ";
+            // 
+            // DataGrid
+            // 
+            this.DataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DataGrid.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid.Location = new System.Drawing.Point(41, 170);
+            this.DataGrid.Name = "DataGrid";
+            this.DataGrid.RowHeadersWidth = 51;
+            this.DataGrid.RowTemplate.Height = 24;
+            this.DataGrid.Size = new System.Drawing.Size(742, 291);
+            this.DataGrid.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(822, 495);
+            this.Controls.Add(this.DataGrid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Confirm);
+            this.Controls.Add(this.Input);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "A2 Access Database";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox Input;
+        private System.Windows.Forms.Button Confirm;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DataGrid;
+    }
+}
+
---
+++ A2_ADB_Athena/A2_AccessDatabase/A2_AccessDatabase/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A2_AccessDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void AccessDatabase()
+        {
+            String ConnStr = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\athen\\Desktop\\FarmInfo.accdb;
+                            Persist Security Info = False";
+            String error_Msg = "Error! No data found.";
+            String q = Input.Text;
+            String query = "Select * from " + q + ";"; // will hold the query
+            OleDbConnection conn = null; // declaring variable connection 
+
+            try
+            {
+                conn = new OleDbConnection(ConnStr); //create object - connection string 
+                conn.Open(); // starts the actual connection
+                OleDbCommand cmd = new OleDbCommand(query, conn)
+                {
+                    Connection = conn,
+                    CommandText = query
+                };
+
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataTable dataTable = new DataTable();
+                da.Fill(dataTable);
+                DataGrid.DataSource = dataTable;
+
+                conn.Close(); //close connection
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(error_Msg); //will display error message if query is invalid
+            }
+        }
+
+        private void Confirm_Click(object sender, EventArgs e)
+        {
+            AccessDatabase();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Input.Clear();
+            this.DataGrid.DataSource = null;
+            this.DataGrid.Rows.Clear();
+        }
+    }
+}
